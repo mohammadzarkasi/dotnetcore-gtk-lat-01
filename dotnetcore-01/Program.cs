022f1283e72aa1f7b753c40ecc165c642866d5af
mohammadzarkasi <mohammad.zarkasi@gmail.com>
2021-02-06 10:47:22 +0700

    init project

---
+++ dotnetcore-01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+
+namespace dotnetcore_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Application.Init();
+
+            var w = new Window("gtk 01");
+            w.Show();
+
+            w.DeleteEvent += W_DeleteEvent;
+
+            Application.Run();
+        }
+
+        private static void W_DeleteEvent(object o, DeleteEventArgs args)
+        {
+            Application.Quit();
+        }
+    }
+}
