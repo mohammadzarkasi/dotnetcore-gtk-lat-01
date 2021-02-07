33cd7bbf1b408176bd98b2bf7b1dab540ff10430
mohammadzarkasi <mohammad.zarkasi@gmail.com>
2021-02-07 12:44:12 +0700

    print beberapa info

--- dotnetcore-01/Program.cs
+++ dotnetcore-01/Program.cs
@@ -7,17 +7,22 @@ namespace dotnetcore_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("initializing gtk....");
 
             Application.Init();
+            Console.WriteLine("done");
 
+            Console.Write("\ncreating window...");
             var w = new Window("gtk 01");
             w.SetDefaultSize(900, 500);
             w.Show();
+            Console.WriteLine("done");
 
             w.DeleteEvent += W_DeleteEvent;
 
+            Console.Write("\nrun the app...");
             Application.Run();
+            Console.WriteLine("done");
         }
 
         private static void W_DeleteEvent(object o, DeleteEventArgs args)
